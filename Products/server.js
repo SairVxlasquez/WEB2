d1e25d49356b4ce2473441b58257fa8603f86e87
Your Name <spaiser12345@gmail.com>
2026-01-30 19:23:19 -0600

    Implemented  PUT route using :id parameter, Updates only sent fields (name, category, subcategory, price, stock, rating) it does  basic validation for ID + 404 if product not found as well, returns updated product on success (200)

--- Products/server.js
+++ Products/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const fs = require('fs')
 
+
 function hasContent(str){
     return typeof str === 'string' && str.length > 0 
 }
@@ -48,7 +49,7 @@ app.post("/products", (req, res) => {
         price: Number(price),
         currency: currency || "USD",
         stock: Number(stock) || 0,
-        rating: Number(rating)
+        rating: Number(rating) || 0
     };
     products.push(newProduct);
     data.count = products.length;
@@ -56,7 +57,36 @@ app.post("/products", (req, res) => {
     fs.writeFileSync("./products.json", JSON.stringify(data, null, 2), {encoding: "utf-8"});
     
     res.status(201).json(newProduct);
-})
+});
+
+app.put("/products/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    
+    if (isNaN(id)) {
+        return res.status(400).json({ error: "Invalid ID" });
+    }
+
+    let data = JSON.parse(fs.readFileSync("./products.json", "utf-8"));
+    let products = data.products || [];
+
+    let product = products.find(p => p.id === id);
+    
+    if (!product) {
+        return res.status(404).json({ error: "Product not found" });
+    }
+    if (req.body.name)        product.name        = req.body.name;
+    if (req.body.category)    product.category    = req.body.category;
+    if (req.body.subcategory) product.subcategory = req.body.subcategory;
+    if (req.body.price)       product.price       = Number(req.body.price);
+    if (req.body.rating)      product.rating      = Number(req.body.rating);
+    if (req.body.stock)       product.stock       = Number(req.body.stock);
+
+    fs.writeFileSync("./products.json", JSON.stringify(data, null, 2));
+
+    res.status(200).json({ message: `Product ${id} updated successfully` });
+});
+
+
  app.listen(9000, () =>  console.log("Server started on http://localhost:9000"))
 
 
